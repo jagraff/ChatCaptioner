6924664b61c7932b02409bef38397579268b95d6
Jacob Graff <jacob@giphy.com>
2023-04-19 13:28:31 -0400

    Add more question customization

--- chatcaptioner/chat.py
+++ chatcaptioner/chat.py
@@ -10,6 +10,8 @@ from tenacity import (
 )  # for exponential backoff
 import gradio as gr
 
+from PIL import Image, ImageSequence
+
 from chatcaptioner.blip2 import Blip2
 from chatcaptioner.utils import print_info, plot_img
 
@@ -19,12 +21,13 @@ QUESTION_INSTRUCTION = \
 "Ask me questions about the content of this image. " \
 "Carefully asking me informative questions to maximize your information about this image content. " \
 "Each time ask one question only without giving an answer. " \
-"Avoid asking yes/no questions." \
+"Avoid asking yes/no questions. " \
+"Don't repeat any questions. " \
 "I'll put my answer beginning with \"Answer:\"." \
 
 
 SUB_QUESTION_INSTRUCTION = \
-"Next Question. Avoid asking yes/no questions. \n" \
+"Next Question. \n" \
 "Question: "
 
 
@@ -44,7 +47,7 @@ SUB_ANSWER_INSTRUCTION = 'Answer: '  # template following blip2 huggingface demo
 FIRST_QUESTION = 'Describe this image in detail.'
 
 
-VALID_CHATGPT_MODELS = ['gpt-3.5-turbo']
+VALID_CHATGPT_MODELS = ['gpt-3.5-turbo', 'gpt-4']
 VALID_GPT3_MODELS = ['text-davinci-003', 'text-davinci-002', 'davinci']
 
 
@@ -98,6 +101,7 @@ def prepare_gpt_prompt(task_prompt, questions, answers, sub_prompt):
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 def call_gpt3(gpt3_prompt, max_tokens=40, model="text-davinci-003"):  # 'text-curie-001' does work at all to ask questions
     response = openai.Completion.create(model=model, prompt=gpt3_prompt, max_tokens=max_tokens)  # temperature=0.6, 
+
     reply = response['choices'][0]['text']
     total_tokens = response['usage']['total_tokens']
     return reply, total_tokens
@@ -125,12 +129,25 @@ def call_chatgpt(chatgpt_messages, max_tokens=40, model="gpt-3.5-turbo"):
 
 class AskQuestions():
 
-    def __init__(self, img, blip2, model, max_gpt_token=30, n_blip2_context=-1):
+    def __init__(self, img, blip2, model, max_gpt_token=30, n_blip2_context=-1,
+                 tags=[],
+                 question=QUESTION_INSTRUCTION,
+                 sub_question=SUB_QUESTION_INSTRUCTION,
+                 summary=SUMMARY_INSTRUCTION,
+                 answer=ANSWER_INSTRUCTION,
+                 first_question=FIRST_QUESTION
+                 ):
         self.img = img
         self.blip2 = blip2
         self.model = model
         self.max_gpt_token = max_gpt_token
         self.n_blip2_context = n_blip2_context
+
+        self.question = question
+        self.sub_question = sub_question
+        self.summary = summary
+        self.answer = answer
+        self.first_question = first_question
 
         self.questions = []
         self.answers = []
@@ -142,33 +159,45 @@ class AskQuestions():
         self.answers = []
         self.total_tokens = 0
 
+    def get_instructions(self):
+        instructions_dict = {
+            'question': self.question,
+            'sub_question': self.sub_question,
+            'summary': self.summary,
+            'answer': self.answer,
+            'sub_answer': self.sub_answer,
+            'first_question': self.first_question 
+        }
+
+        return instructions_dict
+
     def ask_question(self):
         if len(self.questions) == 0:
             # first question is given by human to request a general discription
-            question = FIRST_QUESTION
+            question = self.first_question
         else:
             if self.model in VALID_CHATGPT_MODELS:
                 chatgpt_messages = prepare_chatgpt_message(
-                    QUESTION_INSTRUCTION,
+                    self.question,
                     self.questions, self.answers,
-                    SUB_QUESTION_INSTRUCTION
+                    self.sub_question
                 )
                 question, n_tokens = call_chatgpt(chatgpt_messages, model=self.model, max_tokens=self.max_gpt_token)
             elif self.model in VALID_GPT3_MODELS:
                 # prepare the context for GPT3
                 gpt3_prompt = prepare_gpt_prompt(
-                    QUESTION_INSTRUCTION,
+                    self.question,
                     self.questions, self.answers,
-                    SUB_QUESTION_INSTRUCTION
+                    self.sub_question
                 )
 
                 question, n_tokens = call_gpt3(gpt3_prompt, model=self.model, max_tokens=self.max_gpt_token)
             elif isinstance(self.model, Blip2):
                 # prepare the context for other LLM
                 gpt_prompt = prepare_gpt_prompt(
-                    QUESTION_INSTRUCTION,
+                    self.question,
                     self.questions, self.answers,
-                    SUB_QUESTION_INSTRUCTION
+                    self.sub_question
                 )
                 n_tokens = 0  # local model. no token cost on OpenAI API.
                 question = self.model.call_llm(gpt_prompt)
@@ -191,9 +220,9 @@ class AskQuestions():
 
     def answer_question(self):
         # prepare the context for blip2
-        blip2_prompt = '\n'.join([ANSWER_INSTRUCTION,
+        blip2_prompt = '\n'.join([self.answer,
                                   get_chat_log(self.questions, self.answers, last_n=self.n_blip2_context),
-                                  SUB_ANSWER_INSTRUCTION])
+                                  self.sub_answer])
 
         answer = self.blip2.ask(self.img, blip2_prompt)
         return answer
@@ -231,25 +260,65 @@ class AskQuestions():
 
         return self.questions, self.answers, self.total_tokens
 
+    def summarize(self):
+        if self.model in VALID_GPT3_MODELS:
+            summary_prompt = prepare_gpt_prompt(
+                        self.quetion,
+                        self.questions, self.answers,
+                        self.summary
+                    )
+            summary, n_tokens = call_gpt3(summary_prompt, model=self.model, max_tokens=self.max_gpt_token)
+        elif self.model in VALID_CHATGPT_MODELS:
+            summary_prompt = prepare_chatgpt_message(
+                        self.quetion,
+                        self.questions, self.answers,
+                        self.summary
+                    )
+            summary, n_tokens = call_chatgpt(summary_prompt, model=self.model, max_tokens=self.max_gpt_token)
+        elif isinstance(self.model, Blip2):
+            summary_prompt = prepare_gpt_prompt(
+                        self.question,
+                        self.questions, self.answers,
+                        self.summary 
+                    )
+            n_tokens = 0 # local model. no token cost on OpenAI API.
+            summary = self.model.call_llm(summary_prompt)
+        else:
+            raise ValueError('{} is not a valid question model'.format(model))
 
-def summarize_chat(questions, answers, model, max_gpt_token=100):
+        summary = summary.replace('\n', ' ').strip()
+        return summary, summary_prompt, n_tokens
+
+
+def summarize_chat(questions, answers, model, max_gpt_token=100, tags=[]):
+    question_instruction = QUESTION_INSTRUCTION
+    if len(tags) > 0:
+        question_instruction = "I have an image. " \
+                               "Ask me questions about the content of this image. " \
+                               "Carefully ask me informative questions to maximize your information about this image content. " \
+                               "Each time ask one question only without giving an answer. " \
+                               f"Some (but not all) of this list of tags might be relevant to the image: '{tags}'. " \
+                               "Make sure to ask some questions about the tags. " \
+                               "Don't repeat any questions. " \
+                               "I'll put my answer beginning with \"Answer:\"." \
+
     if model in VALID_GPT3_MODELS:
         summary_prompt = prepare_gpt_prompt(
-                    QUESTION_INSTRUCTION, 
+                    question_instruction, 
                     questions, answers, 
                     SUMMARY_INSTRUCTION)
 
         summary, n_tokens = call_gpt3(summary_prompt, model=model, max_tokens=max_gpt_token)
     elif model in VALID_CHATGPT_MODELS:
         summary_prompt = prepare_chatgpt_message(
-                    QUESTION_INSTRUCTION, 
+                    question_instruction, 
                     questions, answers, 
                     SUMMARY_INSTRUCTION
                 )
         summary, n_tokens = call_chatgpt(summary_prompt, model=model, max_tokens=max_gpt_token)
     elif isinstance(model, Blip2):
         summary_prompt = prepare_gpt_prompt(
-                    QUESTION_INSTRUCTION, 
+                    question_instruction, 
                     questions, answers, 
                     SUMMARY_INSTRUCTION
                 )
@@ -257,7 +326,7 @@ def summarize_chat(questions, answers, model, max_gpt_token=100):
         summary = model.call_llm(summary_prompt)
     else:
         raise ValueError('{} is not a valid question model'.format(model))
-        
+
     summary = summary.replace('\n', ' ').strip()
     return summary, summary_prompt, n_tokens
 
@@ -283,7 +352,52 @@ def caption_image(blip2, image, model, n_rounds=10, n_blip2_context=-1, print_mo
     # Default BLIP2 caption
     caption = blip2.caption(image)
     results['BLIP2'] = {'caption': caption}
-    
+
+    return results
+
+
+def caption_gif(blip2, gif_path, tags, model, n_rounds=10, n_blip2_context=-1, print_mode='no',
+                question=QUESTION_INSTRUCTION,
+                sub_question=SUB_QUESTION_INSTRUCTION,
+                summary=SUMMARY_INSTRUCTION,
+                answer=ANSWER_INSTRUCTION,
+                first_question=FIRST_QUESTION):
+    if model == 'gpt3':
+        model = 'text-davinci-003'
+    elif model == 'chatgpt':
+        model = 'gpt-3.5-turbo'
+
+    gif = Image.open(gif_path)
+
+    frames = []
+    for frame in ImageSequence.Iterator(gif):
+        new_frame = frame.convert(mode="RGB")
+        frames.append(new_frame)
+
+    image = frames[int(len(frames)/2)]
+
+    results = {}
+    chat = AskQuestions(image,
+                        blip2,
+                        n_blip2_context=n_blip2_context,
+                        model=model,
+                        tags=tags,
+                        question=question,
+                        sub_question=sub_question,
+                        summary=summary,
+                        answer=answer,
+                        first_question=first_question)
+
+    questions, answers, n_token_chat = chat.chatting(n_rounds+len(tags), print_mode=print_mode)
+
+    summary, summary_prompt, n_token_sum = chat.summarize()
+    results['ChatCaptioner'] = {'caption': summary, 'chat': summary_prompt, 'n_token': n_token_chat + n_token_sum}
+    results['BLIP2+OurPrompt'] = {'caption': answers[0]}
+
+    # Default BLIP2 caption
+    caption = blip2.caption(image)
+    results['BLIP2'] = {'caption': caption}
+
     return results
 
 
@@ -307,14 +421,14 @@ def caption_images(blip2s, dataset, img_ids, model, save_path='', n_rounds=10, n
         model = 'text-davinci-003'
     elif model == 'chatgpt':
         model = 'gpt-3.5-turbo'
-    
+
     for img_id in tqdm(img_ids, disable=print_mode!='no'):
         caption_path = os.path.join(save_path, 'caption_result', '{}.yaml'.format(img_id))
         if os.path.exists(caption_path):
             continue
         if print_mode != 'no':
             print('Image ID {}'.format(img_id))
-            
+
         image, gt_captions = dataset.fetch_img(img_id)
         info = {'setting':
                     {'dataset': dataset.name,
